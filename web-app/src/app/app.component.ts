1aa87a9686ea41b1ae05dfd41423298e46625ca6
Praveen Muthukumarana <praveenmuth2@gmail.com>
2020-06-10 13:01:30 +0530

    RXJS operators basics

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CurrencyUtilsService } from './common/currency-utils.service';
+import { Observable, range } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +9,17 @@ import { CurrencyUtilsService } from './common/currency-utils.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  value = 19.99;
-  constructor(private currencyService: CurrencyUtilsService) { }
+  source$: Observable<number> = range(0, 5);
 
-  test(): void{
-    this.value = this.currencyService.convertUSDtoLKR(this.value);
+  test1(): void {
+    this.source$.pipe(
+      map(x => x * 3)
+    ).subscribe(x => console.log(x));
+  }
+
+  test2(): void {
+    this.source$.pipe(
+      filter(x => x % 2 === 0)
+    ).subscribe(x => console.log(x));
   }
 }
